70f3fd2eea4365dd3aa4344522c8f4de80d46978
Martin Zahnd <mzahnd@itba.edu.ar>
2022-10-11 08:11:29 -0300

    Add somemcpy test * Code readability: variable name change in test_somemset

--- test/Kernel/mem/driver.c
+++ test/Kernel/mem/driver.c
@@ -13,6 +13,7 @@
 #define WRITTEN_VALUE '#'
 
 void test_somemset(CuTest *const ct);
+void test_somemcpy(CuTest *const ct);
 
 CuSuite *test_get_driver_suite(void)
 {
@@ -20,50 +21,106 @@ CuSuite *test_get_driver_suite(void)
 
         /* somemset */
         SUITE_ADD_TEST(suite, test_somemset);
+        SUITE_ADD_TEST(suite, test_somemcpy);
 
         return suite;
 }
 
 void test_somemset(CuTest *const ct)
 {
-        uint8_t *buffer = (uint8_t *)calloc(TEST_MEMORY_SIZE, sizeof(uint8_t));
-        if (buffer == NULL) {
+        uint8_t *test_buffer =
+                (uint8_t *)calloc(TEST_MEMORY_SIZE, sizeof(uint8_t));
+        if (test_buffer == NULL) {
                 perror("malloc");
                 exit(1);
         }
 
         // Also test out of bounds writting (to either side)
-        uint8_t *shifted_buffer_addr = (uint8_t *)buffer + 128;
+        uint8_t *shifted_test_buffer_addr = (uint8_t *)test_buffer + 128;
         uint8_t *returned_ptr = (uint8_t *)somemset(
-                shifted_buffer_addr, WRITTEN_VALUE, TEST_MEMORY_SIZE / 4);
+                shifted_test_buffer_addr, WRITTEN_VALUE, TEST_MEMORY_SIZE / 4);
 
         CuAssertPtrNotNull(ct, returned_ptr);
-        CuAssertPtrEquals(ct, shifted_buffer_addr, returned_ptr);
+        CuAssertPtrEquals(ct, shifted_test_buffer_addr, returned_ptr);
 
-        // Test that no writting has been performed before shifted_buffer_addr
-        for (uint8_t *ptr = buffer; ptr < shifted_buffer_addr; ptr++) {
+        // Test that no writting has been performed before
+        // shifted_test_buffer_addr
+        for (uint8_t *ptr = test_buffer; ptr < shifted_test_buffer_addr;
+             ptr++) {
                 CuAssertIntEquals(ct, 0, *ptr);
         }
 
-        // Test that no writting has been performed after shifted_buffer_addr
-        // plus the given buffer size
+        // Test that no writting has been performed after
+        // shifted_test_buffer_addr
+        // plus the given test_buffer size
         for (uint8_t *ptr =
-                     (uint8_t *)shifted_buffer_addr + TEST_MEMORY_SIZE / 4;
-             ptr < (uint8_t *)buffer + TEST_MEMORY_SIZE; ptr++) {
+                     (uint8_t *)shifted_test_buffer_addr + TEST_MEMORY_SIZE / 4;
+             ptr < (uint8_t *)test_buffer + TEST_MEMORY_SIZE; ptr++) {
                 CuAssertIntEquals(ct, 0, *ptr);
         }
 
-        char *ok_buffer =
-                (char *)calloc(TEST_MEMORY_SIZE / 4 + 1, sizeof(char));
+        uint8_t *ok_buffer =
+                (uint8_t *)calloc(TEST_MEMORY_SIZE / 4 + 1, sizeof(char));
         if (ok_buffer == NULL) {
                 perror("malloc");
                 exit(1);
         }
 
-        CuAssertStrEquals(
-                ct, memset(ok_buffer, WRITTEN_VALUE, TEST_MEMORY_SIZE / 4),
-                (char *)shifted_buffer_addr);
+        CuAssertStrEquals(ct,
+                          (char *)memset(ok_buffer, WRITTEN_VALUE,
+                                         TEST_MEMORY_SIZE / 4),
+                          (char *)shifted_test_buffer_addr);
 
         free(ok_buffer);
-        free(buffer);
+        free(test_buffer);
+}
+
+void test_somemcpy(CuTest *const ct)
+{
+        uint8_t *test_buffer =
+                (uint8_t *)calloc(TEST_MEMORY_SIZE, sizeof(uint8_t));
+        if (test_buffer == NULL) {
+                perror("malloc");
+                exit(1);
+        }
+
+        uint8_t *ok_buffer =
+                (uint8_t *)calloc(TEST_MEMORY_SIZE / 4 + 1, sizeof(char));
+        if (ok_buffer == NULL) {
+                perror("malloc");
+                exit(1);
+        }
+
+        memset(ok_buffer, WRITTEN_VALUE, TEST_MEMORY_SIZE / 4);
+
+        // Test out of bounds writting (to either side)
+        uint8_t *shifted_test_buffer_addr =
+                (uint8_t *)test_buffer + TEST_MEMORY_SIZE / 8;
+        uint8_t *returned_ptr = (uint8_t *)somemcpy(
+                shifted_test_buffer_addr, ok_buffer, TEST_MEMORY_SIZE / 4);
+
+        CuAssertPtrNotNull(ct, returned_ptr);
+        CuAssertPtrEquals(ct, shifted_test_buffer_addr, returned_ptr);
+
+        // Test that no writting has been performed before
+        // shifted_test_buffer_addr
+        for (uint8_t *ptr = test_buffer; ptr < shifted_test_buffer_addr;
+             ptr++) {
+                CuAssertIntEquals(ct, 0, *ptr);
+        }
+
+        // Test that no writting has been performed after
+        // shifted_test_buffer_addr
+        // plus the given test_buffer size
+        for (uint8_t *ptr =
+                     (uint8_t *)shifted_test_buffer_addr + TEST_MEMORY_SIZE / 4;
+             ptr < (uint8_t *)test_buffer + TEST_MEMORY_SIZE; ptr++) {
+                CuAssertIntEquals(ct, 0, *ptr);
+        }
+
+        CuAssertStrEquals(ct, (char *)ok_buffer,
+                          (char *)shifted_test_buffer_addr);
+
+        free(ok_buffer);
+        free(test_buffer);
 }

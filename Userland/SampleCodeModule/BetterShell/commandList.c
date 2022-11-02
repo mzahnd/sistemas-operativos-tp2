9ea16cecfb54b2868cc5065522eedad29659fc48
Pedro Lopez <pdihax@gmail.com>
2022-11-02 17:30:37 -0300

    CommandList ready and Shell getting commands from list

--- Userland/SampleCodeModule/BetterShell/commandList.c
+++ Userland/SampleCodeModule/BetterShell/commandList.c
@@ -1,58 +1,66 @@
-#include "../include/BetterShell/commandList.h"
-#include "../include/stdlib.h"
-// #include "commandList.h"
+#include <BetterShell/commandList.h>
+#include <stdlib.h>
+#include <stringUtils.h>
 
-typedef struct commandList {
-        commandNode_t *firstCommand;
-        commandNode_t *currentCommand;
-} commandList;
-
-typedef processMainFunction_t int (*)(int, char **);
+commandNode_t *addCommandRec(commandNode_t *aux, char *name, processMainFunction_t function, unsigned int* sizePtr);
 
 commandList newCommandList()
 {
-        return calloc(1, sizeof(commandList));
+	commandList list = malloc(sizeof(commandList_t));
+	list->firstCommand = NULL;
+	list->currentCommand = NULL;
+	list->size = 0;
+        return list;
 }
 
 void addCommand(commandList commandLinkedList, char *name,
-                processMainFunction_t funtion)
+                processMainFunction_t function)
 {
+	if (commandLinkedList == NULL) {
+		return;
+	}
         commandLinkedList->firstCommand =
-                addCommandRec(commandLinkedList->firstCommand, name, fuction);
+                addCommandRec(commandLinkedList->firstCommand, name, function, &(commandLinkedList->size));
 }
 
 commandNode_t *addCommandRec(commandNode_t *aux, char *name,
-                             processMainFunction_t funtion)
+                             processMainFunction_t function, unsigned int* sizePtr)
 {
-        if (firstCommand == NULL) {
+        if (aux == NULL) {
                 commandNode_t *newCommand = malloc(sizeof(commandNode_t));
                 newCommand->name = name;
-                newCommand->next = aux;
+                newCommand->next = NULL;
                 newCommand->command = function;
+		(*sizePtr)++; // Increase the size by 1
                 return newCommand;
         }
-        aux->next = addCommandRec(aux->tail, name, function);
+        aux->next = addCommandRec(aux->next, name, function, sizePtr);
         return aux;
 }
 
-//Un get al que le pasas el nombre y te devuelve el puntero
-//a la funcion, y que por parametro de salida te devuelva el tipo de comando.
-//Si no esta el comando, retorna NULL.
-
-void getFunction(commandList commandLinkedList, char *name, int type)
-{
-        commandLinkedList->firstCommand = getFunctionRec(name, type);
-}
-
-processMainFunction_t *getFunctionRec(char *name, int type)
-{
-        if (currentCommand == NULL) {
+processMainFunction_t getCommand(commandList list, char* name, unsigned int* type) {
+	if (list == NULL || list->size == 0) {
+		return NULL;
+	}
+	commandNode node = list->firstCommand;
+        while (node && strcmp(name, node->name) != 0) {
+                node = node->next;
+        }
+        if (!node) {
                 return NULL;
         }
+        *type = node->type;
+        return node->command;
+}
 
-        if (currentCommand.name == name) {
-                type = currentCommand.type;
-                return currentCommand.(*command)(int, char **);
+int testA(int argc, char** argv) {
+        printf("He vivido\n");
+        return 0;
+}
+
+void initProcesses(commandList list) {
+        if (list == NULL) {
+                return;
         }
-        currentCommand->next = getFunctionRec(name, &type);
+        addCommand(list, "test", testA);
 }
--- Userland/SampleCodeModule/include/BetterShell/commandList.h
+++ Userland/SampleCodeModule/include/BetterShell/commandList.h
@@ -4,11 +4,28 @@
 #define PROCESS_COMMAND 1
 #define FUNCTION_COMMAND 2
 
+
+typedef int (*processMainFunction_t)(int, char **);
+
 typedef struct commandNode_t {
         struct commandNode_t *next;
         char *name;
-        int (*command)(int, char **);
+        processMainFunction_t command;
         int type;
-};
+} commandNode_t;
 
+typedef struct commandList_t {
+        commandNode_t *firstCommand;
+        commandNode_t *currentCommand;
+	unsigned int size;
+} commandList_t;
+
+typedef commandNode_t * commandNode;
+typedef commandList_t * commandList;   
+
+commandList newCommandList();
+void addCommand(commandList commandLinkedList, char *name, processMainFunction_t funtion);
+processMainFunction_t getCommand(commandList list, char* name, unsigned int* type);
+
+void initProcesses(commandList list);
 #endif

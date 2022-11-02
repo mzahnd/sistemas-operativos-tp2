900d2ec513816d3fd6a427414b74697a3d313ab7
Martin Zahnd <mzahnd@itba.edu.ar>
2022-11-02 19:33:55 -0300

    Make sure we're in the script directory on run

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+readonly SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
+pushd "$SCRIPT_DIR" &> /dev/null
+
 if [[ "$1" = "gdb" ]]; then
     qemu-system-x86_64 -s -S -d int -hda Image/x64BareBonesImage.qcow2 -m 512 -rtc base=localtime
 else
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -rtc base=localtime 
 fi
+
+popd &> /dev/null # $SCRIPT_DIR

13d25fef3b52b439f6c4af6628446545b936e6bf
Martin Zahnd <mzahnd@itba.edu.ar>
2022-11-02 19:28:50 -0300

    Fix bugs. Add license
    - A home path was hardcoded (replaced with $HOME)
    - mkdir /itba/ to avoid not-obvios gdb problem while debugging
    - Quote variables
    - Make sure we're in the script directory while running

--- env/run_debug.sh
+++ env/run_debug.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+# SPDX-License-Identifier: MIT
+# 
+# Copyright (c) 2022 López, P.
+#                    Zahnd, M. E.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+readonly SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
+pushd "$SCRIPT_DIR" &> /dev/null
+
+cd .. # Running in project/env/, but needed in project/
+
 if [ $# -eq 0 ]; then
     echo "Must pass Local IP as Argument"
     exit
@@ -11,13 +39,16 @@ if [ -f /.dockerenv ]; then
 else
     echo "Debugging on host";
     sudo rm -rf /itba/*
+    sudo mkdir /itba/
 fi
 
 echo $HOME
 
-cat .gdbinit > /home/pedro/.gdbinit
+cat .gdbinit > "$HOME/.gdbinit"
 
-sed -i "1 s/.*/target remote ${MY_IP}:1234/" ${HOME}/.gdbinit
+sed -i "1 s/.*/target remote ${MY_IP}:1234/" "${HOME}/.gdbinit"
 
-sudo cp -r -f ${PWD}/* /itba/
+sudo cp -r -f "${PWD}"/* /itba/
 gdb
+
+popd &> /dev/null # $SCRIPT_DIR

efa05774823b31585b1c1cda94125373472d29cc
Martin Zahnd <mzahnd@itba.edu.ar>
2022-11-07 19:15:38 -0300

    Reduced heap size and offset

--- Kernel/include/mem/memory.h
+++ Kernel/include/mem/memory.h
@@ -14,12 +14,12 @@
 #include <stddef.h> /* size_t */
 #include <stdint.h>
 
-// Pure64 free memory starts at 0x100000. Leave 256 MiB for non-heap stuff
-#define MEM_HEAP_START_ADDR (0x100000 << 8)
+// Pure64 free memory starts at 0x100000. Leave 64 MiB for non-heap stuff
+#define MEM_HEAP_START_ADDR (0x100000 << 6)
 
 // HEAP_SIZE can be externally defined
 #ifndef MEM_HEAP_SIZE
-#define MEM_HEAP_SIZE (256 * 1024 * 1024) // 256 MiB
+#define MEM_HEAP_SIZE (64 * 1024 * 1024) // 64 MiB
 #endif
 
 typedef struct {

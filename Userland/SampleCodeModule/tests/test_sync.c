e3e1db1b7c3a092222607851b83d53279182a04f
Pedro <pdihax@gmail.com>
2022-11-10 21:37:13 -0300

    test_sync not working

--- Userland/SampleCodeModule/tests/test_sync.c
+++ Userland/SampleCodeModule/tests/test_sync.c
@@ -13,7 +13,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <tests/test_util.h>
-/* #include "syscall.h" */
+#include <stringUtils.h>
 #include <stdlib.h> /* NULL */
 #include <processManagement.h> /* createProcess(); */
 #include <processes.h> /* commandBlock(); commandKill(); commandNice();
@@ -22,9 +22,9 @@
                             * sem_close(); */
 
 #define SEM_ID "sem"
-#define TOTAL_PAIR_PROCESSES 2
+#define TOTAL_PAIR_PROCESSES 3
 
-int64_t global; //shared memory
+int64_t global = 4; //shared memory
 
 void slowInc(int64_t *p, int64_t inc)
 {
@@ -32,31 +32,44 @@ void slowInc(int64_t *p, int64_t inc)
         giveUpCPU(); //This makes the race condition highly probable
         aux += inc;
         *p = aux;
+        //printf("Ready [%d] by [%d]\n", getPid(), inc);
 }
 
 int my_process_inc(int argc, char *argv[])
 {
         uint64_t n;
-        int8_t inc;
-        int8_t use_sem;
+        int64_t inc;
+        uint64_t use_sem;
         sem_t *sem = NULL;
 
-        if (argc != 3)
-                return -1;
 
-        if ((n = satoi(argv[0])) <= 0)
+        if (argc != 3) {
+                printf("Error: Invalid number of arguments\n");
                 return -1;
-        if ((inc = satoi(argv[1])) == 0)
-                return -1;
-        if ((use_sem = satoi(argv[2])) < 0)
-                return -1;
+        }
 
-        if (use_sem)
+        if ((n = satoi(argv[1])) <= 0) {
+                printf("Error: Repetitions is not valid\n");
+                return -1;
+        }
+        if (strcmp(argv[2], "1") == 0) {
+                inc = 1;
+        } else {
+                inc = -1;
+        }
+        if ((use_sem = satoi(argv[3])) < 0) {
+                printf("Error: Use sem is not valid\n");
+                return -1;
+        }
+
+        if (use_sem) {
                 sem = sem_open(SEM_ID, 1);
-        if (sem == NULL) {
+        }
+        if (sem == NULL && use_sem) {
                 printf("test_sync: ERROR opening semaphore\n");
                 return -1;
         }
+        printf("Process [%d], n: [%d] - inc: [%s]\n", getPid(), n, inc == 1 ? "1" : "-1");
 
         uint64_t i;
         for (i = 0; i < n; i++) {
@@ -77,28 +90,32 @@ int test_sync(uint64_t argc, char *argv[])
 { //{n, use_sem, 0}
         uint64_t pids[2 * TOTAL_PAIR_PROCESSES];
 
-        if (argc != 2)
+        if (argc != 3) {
                 return -1;
+                printf("Error: Two arguments Needed:\n\t- Number of repetitions\n\t- Semaphore used\n");
+        }
 
-        char *argvDec[] = { argv[0], "-1", argv[1], NULL };
-        char *argvInc[] = { argv[0], "1", argv[1], NULL };
+        char *argvDec[] = { "Dec Process", argv[1], "-1", argv[2], NULL };
+        char *argvInc[] = { "Inc Process", argv[1], "1", argv[2], NULL };
 
+        printf("Main Process n: [%s] - useSem: [%s]\n", argv[1], argv[2]);
         global = 0;
 
         uint64_t i;
         for (i = 0; i < TOTAL_PAIR_PROCESSES; i++) {
                 /* pids[i] = my_create_process("my_process_inc", 3, argvDec); */
-                pids[i] = createProcess("my_process_inc", NULL, 3, argvDec, 0);
+                pids[i] = createProcess("my_process_inc", my_process_inc, 3, argvDec, 0);
                 /* pids[i + TOTAL_PAIR_PROCESSES] = */
                 /*         my_create_process("my_process_inc", 3, argvInc); */
                 pids[i + TOTAL_PAIR_PROCESSES] =
-                        createProcess("my_process_inc", NULL, 3, argvInc, 0);
+                        createProcess("my_process_inc", my_process_inc, 3, argvInc, 0);
         }
 
-        // TODO: What's this?
         for (i = 0; i < TOTAL_PAIR_PROCESSES; i++) {
                 waitPID(pids[i]);
+                // printf("Done [%d]\n", pids[i]);
                 waitPID(pids[i + TOTAL_PAIR_PROCESSES]);
+                // printf("Done [%d]\n", pids[i + TOTAL_PAIR_PROCESSES]);
         }
 
         printf("Final value: %d\n", global);
